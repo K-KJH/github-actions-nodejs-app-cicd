226ad9766a504da6b55cd980a4d9f37f83c1b1d5
Junghoo Kim <2071068@hansung.ac.kr>
2024-12-01 03:13:37 +0900

    commit 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('hello, World!\n');
+  res.end('oss fasdf\n');
 });
 
 server.listen(port, hostname, () => {
